23254a9942472ec4d873520bcf6f058bac58ea98
Pavel <aw.pavel@gmail.com>
2016-12-26 01:27:44 +0200

    ready module 1

---
+++ module1-solution/js/module1.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Pavel on 26.12.2016.
+ */
+(function () {
+    'use strict';
+    angular.module('module1', []).controller('EatingController', EatingController);
+
+    EatingController.$inject = ['$scope'];
+    function EatingController($scope) {
+        $scope.eatForLunch = "";
+        $scope.message = "";
+
+        $scope.countOfTheMeals = function (string) {
+            if (angular.$isEmpty(string)) return 0;
+            return string.split(",").length;
+        }
+
+        $scope.checkIfTooMuch = function () {
+            var count = countOfTheMeals($scope.eatForLunch);
+            if (count == 0) $scope.message = "Please enter data first";
+            else if (count >= 1 && count <=3) $scope.message = "Enjoy!";
+            else $scope.message = "Too much!";
+        }
+
+    };
+})();
